5ffe537f06cef4788a1a653a8d94cfbc765680ef
Uli <uli.buschbaum@yahoo.de>
2014-09-24 13:08:40 +0200

    Added Accelerometer, changed Motor and Timing

---
+++ Accelerometer/main.cpp
@@ -0,0 +1,62 @@
+#include <Arduino.h>
+
+extern HardwareSerial Serial;
+
+int INIT_SECONDS = 30;
+int RUN_SECONDS = 60;
+int MEASUREMENT_INTERVAL = 10;
+
+int get_x_basis();
+
+void setup() {
+    Serial.begin(115200);
+
+    //Set green led to low and red led to high
+    digitalWrite(11, HIGH);
+    digitalWrite(10, LOW);
+
+    //Calibrate accelerometer
+    Serial.println("INITIALIZING");
+    int x_basis = get_x_basis();
+    Serial.println("INITIALZING FINISHED - STARTING LOOP");
+
+    //Set green led to low and red led to high
+    digitalWrite(10, HIGH);
+}
+
+void loop() {
+    Serial.println("STARTED LOOP");
+    //Seconds running the calibration
+    for (int i = 0; i < RUN_SECONDS; i++) {
+        //Time of measurements per second
+        for (long j = 0; j < 1000 / MEASUREMENT_INTERVAL; j++) {
+            int x_acc_voltage = analogRead(7);
+            Serial.println(x_acc_voltage);
+            delayMicroseconds(MEASUREMENT_INTERVAL * 1000);
+        }
+    }
+    
+    Serial.println("FINISHED LOOP");
+    digitalWrite(10, LOW);
+    digitalWrite(11, LOW);
+    //NO-OP
+    while (true) {
+    }
+}
+
+int get_x_basis() {
+    //Seconds running the calibration
+    for (int i = 0; i < INIT_SECONDS; i++) {
+        //Time of measurements per second
+        for (long j = 0; j < 1000 / MEASUREMENT_INTERVAL; j++) {
+            int x_acc_voltage = analogRead(7);
+            Serial.print("i=");
+            Serial.print(i);
+            Serial.print(" j=");
+            Serial.print(j);
+            Serial.print(" x_acc_voltage=");
+            Serial.println(x_acc_voltage);
+            delayMicroseconds(MEASUREMENT_INTERVAL * 1000);
+        }
+    }
+}
--- Motor/main.cpp
+++ Motor/main.cpp
@@ -2,21 +2,60 @@
 
 extern HardwareSerial Serial;
 
-int ENABLE_1 = 10;
-int INPUT_1 = 1;
-int INPUT_2 = 2;
+int ENABLE_1 = 5;
+int INPUT_1 = 0;
+int INPUT_2 = 1;
+int ENABLE_2 = 6;
+int INPUT_3 = 2;
+int INPUT_4 = 3;
 
 void setup() {
 
     pinMode(ENABLE_1, OUTPUT);
     pinMode(INPUT_1, OUTPUT);
     pinMode(INPUT_2, OUTPUT);
+    pinMode(ENABLE_2, OUTPUT);
+    pinMode(INPUT_3, OUTPUT);
+    pinMode(INPUT_4, OUTPUT);
 
 }
 
 void loop() {
-    analogWrite(ENABLE_1, 150);
+
+//    analogWrite(ENABLE_1, 127);
+//    digitalWrite(INPUT_1, LOW);
+//    digitalWrite(INPUT_2, HIGH);
+    digitalWrite(ENABLE_2, HIGH);
+    digitalWrite(INPUT_3, HIGH);
+    digitalWrite(INPUT_4, LOW);
+//    delay(2000);
+    digitalWrite(ENABLE_1, HIGH);
     digitalWrite(INPUT_1, HIGH);
     digitalWrite(INPUT_2, LOW);
+//    delay(2000);
+
+    //    analogWrite(ENABLE_1, 255);
+    //    digitalWrite(INPUT_1, LOW);
+    //    digitalWrite(INPUT_2, HIGH);
+    //    analogWrite(ENABLE_2, 255);
+    //    digitalWrite(INPUT_3, HIGH);
+    //    digitalWrite(INPUT_4, LOW);
+    //    delay(2000);
+    //    
+    //    analogWrite(ENABLE_1, 255);
+    //    digitalWrite(INPUT_1, HIGH);
+    //    digitalWrite(INPUT_2, LOW);
+    //    analogWrite(ENABLE_2, 255);
+    //    digitalWrite(INPUT_3, LOW);
+    //    digitalWrite(INPUT_4, HIGH);
+    //    delay(2000);
+    //    
+    //    analogWrite(ENABLE_1, 255);
+    //    digitalWrite(INPUT_1, HIGH);
+    //    digitalWrite(INPUT_2, LOW);
+    //    analogWrite(ENABLE_2, 255);
+    //    digitalWrite(INPUT_3, HIGH);
+    //    digitalWrite(INPUT_4, LOW);
+    //    delay(2000);
 
 }
