7df408b80cc320520523fd2fa561edf38fbc2a97
Uli <uli.buschbaum@yahoo.de>
2014-12-07 11:24:47 +0100

    SPI Arduino

--- SPIArduino/main.cpp
+++ SPIArduino/main.cpp
@@ -1,18 +1,40 @@
-#include <Arduino.h>
+// Written by Nick Gammon
+// February 2011
+/**
+ * Send arbitrary number of bits at whatever clock rate (tested at 500 KHZ and 500 HZ).
+ * This script will capture the SPI bytes, when a '\n' is recieved it will then output
+ * the captured byte stream via the serial.
+ */
+
 #include "SPI.h"
 
-extern HardwareSerial Serial;
+void setup(void) {
+    Serial.begin(115200); // debugging
 
-void setup() {
-    pinMode(10, OUTPUT);
-    digitalWrite(10, HIGH);
-    SPI.begin();
-    SPI.setBitOrder(MSBFIRST);
+    // have to send on master in, *slave out*
+    //pinMode(MISO, INPUT);
+    //pinMode(MOSI, INPUT);
+    //pinMode(SCK, INPUT);
+
+    //Act as slave
+    SPCR |= _BV(SPE);
+    //Use SPI mode 0 as the raspberry pi does
+    SPI.setDataMode(SPI_MODE0);
 }
 
 void loop() {
-    digitalWrite(10, LOW);
-    SPI.transfer(0xFF);
-    digitalWrite(10, HIGH);
-//    delay(5000);
-}
+    //    if (digitalRead(MISO) == HIGH) {
+    //        Serial.println("MISO is high!");
+    //    }
+    //    if (digitalRead(MOSI) == HIGH) {
+    //        Serial.println("MOSI is high!");
+    //    }
+    //    if (digitalRead(SCK) == HIGH) {
+    //        Serial.println("SCK is high!");
+    //    }
+    
+    int data = 230;
+    int bytes = SPI.transfer(data);
+    Serial.println("Bytes received");
+    Serial.println(bytes);
+} 
