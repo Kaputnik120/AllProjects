77f58bb340d990c620033492a594f7d55305f34f
Uli <uli.buschbaum@yahoo.de>
2014-12-10 18:46:07 +0100

    Successfully reading I2C address register

--- Pathfinder/src/main/java/de/buschbaum/java/pathfinder/Main.java
+++ Pathfinder/src/main/java/de/buschbaum/java/pathfinder/Main.java
@@ -13,113 +13,101 @@ import java.util.Random;
  *
  */
 public class Main {
-
+    
     public static void main(String[] args) {
         try {
-            printSystemInformation();
             System.out.println("Creating I2C bus");
             I2CBus bus = I2CFactory.getInstance(I2CBus.BUS_1);
             System.out.println("Creating I2C device");
             I2CDevice device = bus.getDevice(0x68);
-
-            long waitTimeRead = 250;
-
+            
+            int iterations = 0;
+            
+            String readsPerIterationProp = args[0].substring(2, args[0].length());
+            int readsPerIteration = Integer.parseInt(readsPerIterationProp);
+            
+            String startRegisterProp = args[1].substring(2, args[1].length());
+            byte startRegister = Byte.parseByte(startRegisterProp);
+            
+            String waitReadTimeProp = args[2].substring(2, args[2].length());
+            long waitReadTime = Long.parseLong(waitReadTimeProp);
+            
+            String wakeUpRegisterProp = args[3].substring(2, args[3].length());
+            byte wakeUpRegister = Byte.parseByte(wakeUpRegisterProp);
+            
+            System.out.println("Using readsPerIteration " + readsPerIteration);
+            System.out.println("Using startRegister " + startRegister);
+            
+            System.out.println("Waking device up");
+            device.write(wakeUpRegister);
+            device.write((byte) 0);
+            System.out.println("Device waked up");
+            
+            Thread.sleep(3000);
+            
             while (true) {
-                int dataRead = device.read();
-                System.out.println("Read " + dataRead + " via I2C");
-                Thread.sleep(waitTimeRead);
+
+                //Setting registers
+//                Wire.beginTransmission(MPU);
+//                Wire.write(0x6B);  // PWR_MGMT_1 register
+//                Wire.write(0);     // set to zero (wakes up the MPU-6050)
+//                Wire.endTransmission(true);
+//                device.write((byte) 0x6B);
+//                device.write((byte) 0);
+                //Reading registers
+                if (startRegister > 0) {
+//                    System.out.println("StartRegister sent: " + startRegister);
+                    device.write(startRegister);  // starting with register 0x3B (ACCEL_XOUT_H)
+                } else {
+//                    System.out.println("Not sending StartRegister to MPU6050");
+                }
+
+//
+//                int acX = device.read() << 8 | device.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)    
+//                int acY = device.read() << 8 | device.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+//                int acZ = device.read() << 8 | device.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+//                int tmp = device.read() << 8 | device.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+//                int gyX = device.read() << 8 | device.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+//                int gyY = device.read() << 8 | device.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+//                int gyZ = device.read() << 8 | device.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+//
+//                System.out.println("acX: " + acX);
+//                System.out.println("acY: " + acY);
+//                System.out.println("acZ: " + acZ);
+//                System.out.println("tmp: " + tmp);
+//                System.out.println("gyX: " + gyX);
+//                System.out.println("gyY: " + gyY);
+//                System.out.println("gyZ: " + gyZ);
+                int registers[] = new int[readsPerIteration];
+                for (int i = 0; i < readsPerIteration; i++) {
+                    Thread.sleep(1);
+                    registers[i] = device.read();
+                    System.out.print(registers[i] + "\t");
+                    if (i % 10 == 0) {
+                        System.out.println();
+                    }
+                }
+
+//                System.out.println("Nr. 8: " + registers[8]);
+//                System.out.println("Nr. 13: " + registers[13]);
+//                System.out.println("Nr. 19: " + registers[19]);
+//                System.out.println("Nr. 20: " + registers[20]);
+//                System.out.println("Nr. 21: " + registers[21]);
+//                System.out.println("Nr. 22: " + registers[22]);
+//                System.out.println("Nr. 23: " + registers[23]);
+//                System.out.println("Nr. 24: " + registers[24]);
+//                System.out.println("Nr. 25: " + registers[25]);
+//                System.out.println("Nr. 26: " + registers[26]);
+//                System.out.println();
+//                System.out.println("--------------------------------------------" + iterations + "---------------------------------------------");
+//                System.out.println();
+                iterations++;
+                Thread.sleep(waitReadTime);
             }
         } catch (IOException ex) {
             ex.printStackTrace();
         } catch (InterruptedException ex) {
             ex.printStackTrace();
         }
-    }
-
-    public static void printSystemInformation() throws IOException, InterruptedException {
-        // display a few of the available system information properties
-        System.out.println("----------------------------------------------------");
-        System.out.println("HARDWARE INFO");
-        System.out.println("----------------------------------------------------");
-        System.out.println("Serial Number     :  " + SystemInfo.getSerial());
-        System.out.println("CPU Revision      :  " + SystemInfo.getCpuRevision());
-        System.out.println("CPU Architecture  :  " + SystemInfo.getCpuArchitecture());
-        System.out.println("CPU Part          :  " + SystemInfo.getCpuPart());
-        System.out.println("CPU Temperature   :  " + SystemInfo.getCpuTemperature());
-        System.out.println("CPU Core Voltage  :  " + SystemInfo.getCpuVoltage());
-//        System.out.println("CPU Model Name    :  " + SystemInfo.getModelName());
-//        System.out.println("Processor         :  " + SystemInfo.getProcessor());
-        System.out.println("Hardware Revision :  " + SystemInfo.getRevision());
-        System.out.println("Is Hard Float ABI :  " + SystemInfo.isHardFloatAbi());
-        System.out.println("Board Type        :  " + SystemInfo.getBoardType().name());
-
-        System.out.println("----------------------------------------------------");
-        System.out.println("MEMORY INFO");
-        System.out.println("----------------------------------------------------");
-        System.out.println("Total Memory      :  " + SystemInfo.getMemoryTotal());
-        System.out.println("Used Memory       :  " + SystemInfo.getMemoryUsed());
-        System.out.println("Free Memory       :  " + SystemInfo.getMemoryFree());
-        System.out.println("Shared Memory     :  " + SystemInfo.getMemoryShared());
-        System.out.println("Memory Buffers    :  " + SystemInfo.getMemoryBuffers());
-        System.out.println("Cached Memory     :  " + SystemInfo.getMemoryCached());
-        System.out.println("SDRAM_C Voltage   :  " + SystemInfo.getMemoryVoltageSDRam_C());
-        System.out.println("SDRAM_I Voltage   :  " + SystemInfo.getMemoryVoltageSDRam_I());
-        System.out.println("SDRAM_P Voltage   :  " + SystemInfo.getMemoryVoltageSDRam_P());
-
-        System.out.println("----------------------------------------------------");
-        System.out.println("OPERATING SYSTEM INFO");
-        System.out.println("----------------------------------------------------");
-        System.out.println("OS Name           :  " + SystemInfo.getOsName());
-        System.out.println("OS Version        :  " + SystemInfo.getOsVersion());
-        System.out.println("OS Architecture   :  " + SystemInfo.getOsArch());
-        System.out.println("OS Firmware Build :  " + SystemInfo.getOsFirmwareBuild());
-
-        System.out.println("----------------------------------------------------");
-        System.out.println("JAVA ENVIRONMENT INFO");
-        System.out.println("----------------------------------------------------");
-        System.out.println("Java Vendor       :  " + SystemInfo.getJavaVendor());
-        System.out.println("Java Vendor URL   :  " + SystemInfo.getJavaVendorUrl());
-        System.out.println("Java Version      :  " + SystemInfo.getJavaVersion());
-        System.out.println("Java VM           :  " + SystemInfo.getJavaVirtualMachine());
-        System.out.println("Java Runtime      :  " + SystemInfo.getJavaRuntime());
-
-        System.out.println("----------------------------------------------------");
-        System.out.println("NETWORK INFO");
-        System.out.println("----------------------------------------------------");
-
-        // display some of the network information
-        System.out.println("Hostname          :  " + NetworkInfo.getHostname());
-        for (String ipAddress : NetworkInfo.getIPAddresses()) {
-            System.out.println("IP Addresses      :  " + ipAddress);
-        }
-        for (String fqdn : NetworkInfo.getFQDNs()) {
-            System.out.println("FQDN              :  " + fqdn);
-        }
-        for (String nameserver : NetworkInfo.getNameservers()) {
-            System.out.println("Nameserver        :  " + nameserver);
-        }
-
-        System.out.println("----------------------------------------------------");
-        System.out.println("CODEC INFO");
-        System.out.println("----------------------------------------------------");
-        System.out.println("H264 Codec Enabled:  " + SystemInfo.getCodecH264Enabled());
-        System.out.println("MPG2 Codec Enabled:  " + SystemInfo.getCodecMPG2Enabled());
-        System.out.println("WVC1 Codec Enabled:  " + SystemInfo.getCodecWVC1Enabled());
-
-        System.out.println("----------------------------------------------------");
-        System.out.println("CLOCK INFO");
-        System.out.println("----------------------------------------------------");
-        System.out.println("ARM Frequency     :  " + SystemInfo.getClockFrequencyArm());
-        System.out.println("CORE Frequency    :  " + SystemInfo.getClockFrequencyCore());
-        System.out.println("H264 Frequency    :  " + SystemInfo.getClockFrequencyH264());
-        System.out.println("ISP Frequency     :  " + SystemInfo.getClockFrequencyISP());
-        System.out.println("V3D Frequency     :  " + SystemInfo.getClockFrequencyV3D());
-        System.out.println("UART Frequency    :  " + SystemInfo.getClockFrequencyUART());
-        System.out.println("PWM Frequency     :  " + SystemInfo.getClockFrequencyPWM());
-        System.out.println("EMMC Frequency    :  " + SystemInfo.getClockFrequencyEMMC());
-        System.out.println("Pixel Frequency   :  " + SystemInfo.getClockFrequencyPixel());
-        System.out.println("VEC Frequency     :  " + SystemInfo.getClockFrequencyVEC());
-        System.out.println("HDMI Frequency    :  " + SystemInfo.getClockFrequencyHDMI());
-        System.out.println("DPI Frequency     :  " + SystemInfo.getClockFrequencyDPI());
     }
 }
