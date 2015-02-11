b43ca4a44979275f2264e578793ab0b16dc9ab59
Uli <uli.buschbaum@yahoo.de>
2015-02-11 19:49:43 +0100

    Simple C project created with Netbeans

---
+++ CFirstNetbeansTest/foo.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   foo.h
+ * Author: uli
+ *
+ * Created on 11. Februar 2015, 19:15
+ */
+
+#ifndef FOO_H
+#define	FOO_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+    void foo();
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* FOO_H */
+
