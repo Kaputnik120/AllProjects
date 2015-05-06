0d5074cbef986c1b8aea41a3d6caa2fae141f811
Uli <uli.buschbaum@yahoo.de>
2015-05-06 16:59:55 +0200

    Maven SCP Upload

--- Pathfinder/src/main/java/de/buschbaum/java/pathfinder/Main.java
+++ Pathfinder/src/main/java/de/buschbaum/java/pathfinder/Main.java
@@ -2,6 +2,7 @@ package de.buschbaum.java.pathfinder;
 
 import de.buschbaum.java.pathfinder.device.dcmotors.MotorController;
 import de.buschbaum.java.pathfinder.logic.PositionController;
+import de.buschbaum.java.pathfinder.logic.TimingController;
 
 /**
  * Hello world!
@@ -22,7 +23,33 @@ public class Main {
     }
 
     private static void run() throws Exception {
-        PositionController.update();
+        long started = System.nanoTime();
+        MotorController.driveForward();
+        TimingController.timeSlot(2000, started);
+        started = System.nanoTime();
+        MotorController.stop();
+        TimingController.timeSlot(2000, started);
+        
+        started = System.nanoTime();
+        MotorController.turnLeft();
+        TimingController.timeSlot(2000, started);
+        started = System.nanoTime();
+        MotorController.stop();
+        TimingController.timeSlot(2000, started);
+        
+        started = System.nanoTime();
+        MotorController.driveBackward();
+        TimingController.timeSlot(2000, started);
+        started = System.nanoTime();
+        MotorController.stop();
+        TimingController.timeSlot(2000, started);
+        
+        started = System.nanoTime();
+        MotorController.turnRight();
+        TimingController.timeSlot(2000, started);
+        started = System.nanoTime();
+        MotorController.stop();
+        TimingController.timeSlot(2000, started);
     }
 
     private static void initialize() throws Exception {
