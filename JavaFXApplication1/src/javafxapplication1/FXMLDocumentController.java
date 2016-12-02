bbcf9fe8352144c3f3a57c9899a0efd7c60d4fd0
Ulrich Buschbaum <ulrich.buschbaum@hays.de>
2016-12-02 08:14:18 +0100

    Random stuff

---
+++ JavaFXApplication1/src/javafxapplication1/FXMLDocumentController.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2013 Hays AG
+ */
+package javafxapplication1;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ *
+ * @author ucb
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    @FXML
+    private Label label;
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("You clicked me!");
+        label.setText("Hello World!");
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
---
+++ JavaFXApplication1/src/javafxapplication1/JavaFXApplication1.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2013 Hays AG
+ */
+package javafxapplication1;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author ucb
+ */
+public class JavaFXApplication1 extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
